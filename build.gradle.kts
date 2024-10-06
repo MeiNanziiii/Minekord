b735e4d892356911879fb1c6e975848b9d48ab7f
MeiNanziiii <meinanziiii.ua@gmail.com>
2024-10-06 12:56:43 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,117 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    kotlin("jvm") version "2.0.20"
+    id("fabric-loom") version "1.7.1"
+}
+
+loom {
+    serverOnlyMinecraftJar()
+}
+
+val modVersion: String by project
+val mavenGroup: String by project
+
+base.archivesName.set("minekord")
+
+version = "$modVersion+${libs.versions.minecraft.get()}"
+group = mavenGroup
+
+repositories {
+    maven("https://snapshots-repo.kordex.dev")
+    maven("https://repo.kord.dev/snapshots")
+}
+
+val includeImplementation by configurations.creating {
+    configurations.implementation.configure { extendsFrom(this@creating) }
+}
+
+dependencies {
+    minecraft(libs.minecraft)
+    mappings(libs.yarn)
+
+    modImplementation(libs.fabric.loader)
+    modImplementation(libs.fabric.kotlin)
+    modImplementation(libs.fabric.api)
+
+    includeImplementation(libs.kordex)
+    includeImplementation(libs.konf.core)
+    includeImplementation(libs.konf.toml)
+
+    handleIncludes(includeImplementation)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+tasks {
+    processResources {
+        inputs.property("version", modVersion)
+
+        filesMatching("fabric.mod.json") {
+            expand(
+                "version" to modVersion,
+                "minecraft_version" to libs.versions.minecraft.get(),
+                "loader_version" to libs.versions.fabric.loader.get(),
+                "kotlin_loader_version" to libs.versions.fabric.kotlin.get()
+            )
+        }
+    }
+
+    jar {
+        from("LICENSE")
+    }
+}
+
+/* Thanks to https://github.com/jakobkmar for this script */
+fun DependencyHandlerScope.includeTransitive(
+    dependencies: Set<ResolvedDependency>,
+    fabricLanguageKotlinDependency: ResolvedDependency,
+    checkedDependencies: MutableSet<ResolvedDependency> = HashSet()
+) {
+    val minecraftDependencies = listOf(
+        "icu4j",
+        "slf4j-api",
+        "commons-logging",
+        "oshi-core",
+        "jna",
+        "jna-platform",
+        "gson",
+        "commons-lang3",
+        "jackson-annotations",
+        "jackson-core",
+        "jackson-databind"
+    )
+
+    dependencies.forEach {
+        if (checkedDependencies.contains(it) || it.moduleGroup == "org.jetbrains.kotlin" || it.moduleGroup == "org.jetbrains.kotlinx") return@forEach
+
+        if (fabricLanguageKotlinDependency.children.any { kotlinDep -> kotlinDep.name == it.name }) {
+            println("Skipping -> ${it.name} (already in fabric-language-kotlin)")
+        } else if (minecraftDependencies.any { dep -> dep == it.moduleName }) {
+            println("Skipping -> ${it.name} (already in minecraft)")
+        } else {
+            include(it.name)
+            println("Including -> ${it.name}")
+        }
+        checkedDependencies += it
+
+        includeTransitive(it.children, fabricLanguageKotlinDependency, checkedDependencies)
+    }
+}
+
+fun DependencyHandlerScope.handleIncludes(configuration: Configuration) {
+    includeTransitive(
+        configuration.resolvedConfiguration.firstLevelModuleDependencies,
+        configurations.modImplementation.get().resolvedConfiguration.firstLevelModuleDependencies
+            .first { it.moduleGroup == "net.fabricmc" && it.moduleName == "fabric-language-kotlin" },
+    )
+}
---
+++ src/main/kotlin/ua/mei/minekord/Minekord.kt
@@ -0,0 +1,9 @@
+package ua.mei.minekord
+
+import net.fabricmc.api.ModInitializer
+
+class Minekord : ModInitializer {
+    override fun onInitialize() {
+
+    }
+}
