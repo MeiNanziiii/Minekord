937421bc34603a336485c6cbf89cfe80a6c129c1
MeiNanziiii <201730068+MeiNanziiii@users.noreply.github.com>
2025-06-18 16:58:56 +0300

    final gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin)
+    alias(libs.plugins.kordex)
+    alias(libs.plugins.detekt)
     alias(libs.plugins.loom)
 }
 
@@ -29,6 +31,20 @@ dependencies {
     modImplementation(libs.fabric.loader)
     modImplementation(libs.fabric.api)
     modImplementation(libs.fabric.kotlin)
+
+    implementation(libs.konf.core)
+    implementation(libs.konf.toml)
+}
+
+kordEx {
+    kordVersion = libs.versions.kord
+    kordExVersion = libs.versions.kordex
+}
+
+detekt {
+    buildUponDefaultConfig = true
+
+    config.setFrom("$projectDir/detekt.yml")
 }
 
 java {
@@ -62,5 +78,49 @@ tasks {
 
     jar {
         from("LICENSE")
+    }
+}
+
+afterEvaluate {
+    dependencies {
+        includeTransitive(libs.konf.core.get().toString())
+        includeTransitive(libs.konf.toml.get().toString())
+
+        includeTransitive("dev.kord:kord-core:feat-components-v2-SNAPSHOT")
+        includeTransitive("dev.kordex:kord-extensions:2.3.2-SNAPSHOT")
+    }
+}
+
+fun DependencyHandlerScope.includeTransitive(dependency: String) {
+    val resolvedDependency: ResolvedDependency = configurations.runtimeClasspath.get().resolvedConfiguration.firstLevelModuleDependencies.firstOrNull { it.name == dependency } ?: return
+
+    val minecraftLibraries: Set<ResolvedDependency> = configurations.minecraftLibraries.get().resolvedConfiguration.firstLevelModuleDependencies
+    val kotlinLibraries: Set<ResolvedDependency> = configurations.modImplementation.get().resolvedConfiguration.firstLevelModuleDependencies.first { it.moduleGroup == "net.fabricmc" && it.moduleName == "fabric-language-kotlin" }.children
+    val checkedLibraries: MutableSet<ResolvedDependency> = HashSet()
+
+    includeDependency(resolvedDependency, minecraftLibraries, kotlinLibraries, checkedLibraries)
+}
+
+fun DependencyHandlerScope.includeDependency(
+    dependency: ResolvedDependency,
+    minecraftLibraries: Set<ResolvedDependency>,
+    kotlinLibraries: Set<ResolvedDependency>,
+    checkedLibraries: MutableSet<ResolvedDependency>
+) {
+    if (dependency in checkedLibraries) return
+
+    if (minecraftLibraries.any { it.name == dependency.name }) {
+        println("Skipping -> ${dependency.name} (already in minecraft)")
+    } else if (kotlinLibraries.any { it.name == dependency.name }) {
+        println("Skipping -> ${dependency.name} (already in fabric-language-kotlin)")
+    } else {
+        include(dependency.name)
+        println("Including -> ${dependency.name}")
+    }
+
+    checkedLibraries += dependency
+
+    dependency.children.forEach {
+        includeDependency(it, minecraftLibraries, kotlinLibraries, checkedLibraries)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ pluginManagement {
         maven("https://maven.fabricmc.net/") {
             name = "Fabric"
         }
+        maven("https://repo.kordex.dev/releases")
+        maven("https://repo.kordex.dev/snapshots")
         gradlePluginPortal()
     }
 }
